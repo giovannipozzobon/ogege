258c3947fde138aa86cd3e8e109c4598bf984c77
Curtis Whitley <restday7@gmail.com>
2024-11-24 04:49:40 -0500

    removed commented code

--- src/reg_work/bram_64_kb.v
+++ src/reg_work/bram_64_kb.v
@@ -2,30 +2,7 @@ logic rst_or_clk; assign rst_or_clk = i_rst | i_clk;
 
 reg test;
 reg bram_start;
-/*
-always @(posedge rst_or_clk) begin
-    if (i_rst) begin
-        test <= 1;
-        reg_bram_wea <= 0;
-        reg_bram_web <= 0;
-        reg_bram_clka <= 0;
-        reg_bram_clkb <= 0;
-        reg_bram_put_byte <= 0;
-        reg_bram_dib <= 0;
-        reg_bram_addra <= 0;
-        reg_bram_addrb <= 0;
-        //reg_bram_get_byte;
-        //reg_bram_scan_byte;
-    end else if (test) begin
-        test <= 0;
-        reg_bram_addra <= `RESET_PC_ADDRESS;
-        reg_bram_clka <= 1;
-    end else if (reg_bram_clka) begin
-        reg_bram_clka <= 0;
-    end
-end
-*/
-// this was to test compilation; need to merge this code with code above
+
 always @(posedge rst_or_clk) begin
     logic enable;
     logic do_write;
@@ -39,6 +16,10 @@ always @(posedge rst_or_clk) begin
 
     if (i_rst) begin
         bram_start <= 1;
+        reg_bram_web <= 0;
+        reg_bram_clkb <= 0;
+        reg_bram_dib <= 0;
+        reg_bram_addrb <= 0;
     end else if (delaying) begin
     end else if (bram_start) begin
         bram_start <= 0;
